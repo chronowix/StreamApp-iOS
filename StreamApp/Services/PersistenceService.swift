c920a4b48661564e8895dba3239c5f735b484471
Amélia <amelia.garcia.fern@gmail.com>
2026-02-04 18:51:04 +0100

    Ajout du fichier PersistenceService afin de gérer les sessions utilisateur

---
+++ StreamApp/Services/PersistenceService.swift
@@ -0,0 +1,18 @@
+//
+//  PersistenceService.swift
+//  StreamApp
+//  Created by Alan on 04/02/2026
+//
+
+import Foundation
+
+class PersistenceService {
+    static let shared = PersistenceService()
+    private init() {}
+
+
+    
+
+
+
+}
