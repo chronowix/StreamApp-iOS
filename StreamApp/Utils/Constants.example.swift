163200ca31318aa44144f70ad67c2f3bf40533b1
Amélia <amelia.garcia.fern@gmail.com>
2026-02-04 18:45:18 +0100

    Ajout du fichier exemple pour les constantes

---
+++ StreamApp/Utils/Constants.example.swift
@@ -0,0 +1,11 @@
+//fichier example pour le versioning
+
+import Foundation
+
+struct Constants {
+
+    
+    static let tmdbAPIKey = "VOTRE_CLE_API_ICI"
+    static let tmdbBaseURL = "https://api.themoviedb.org/3"
+    static let tmdbImageBaseURL = "https://image.tmdb.org/t/p/w500"
+}
